a07d518c6ddcec9806289977897aaf69a5fee456
Vitalii Burak <vvitaliy972@gmail.com>
2020-11-10 03:52:51 +0000

    updated users

--- Infosec_group.tf
+++ Infosec_group.tf
@@ -3,3 +3,14 @@ resource "aws_iam_group" "Infosec" {
   path = "/"
 }
 
+resource "aws_iam_group_membership" "Infosec" {
+  name = "Infosec_group"
+
+  users = [
+    "${aws_iam_user.Ron.name}",
+    "${aws_iam_user.Sam.name}",
+    "${aws_iam_user.Billy.name}",
+  ]
+
+  group = "${aws_iam_group.Infosec.name}"
+}
