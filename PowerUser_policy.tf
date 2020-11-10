c06be49379d4eb10f870bf9c2b39f3cb096811a0
Vitalii Burak <vvitaliy972@gmail.com>
2020-11-10 04:06:07 +0000

    policy added

---
+++ PowerUser_policy.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_group_policy" "PowerUser" {
+  name = "PowerUser"
+  group = "${aws_iam_group.Infosec.name}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "NotAction": [
+                "iam:*",
+                "organizations:*",
+                "account:*"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "iam:CreateServiceLinkedRole",
+                "iam:DeleteServiceLinkedRole",
+                "iam:ListRoles",
+                "organizations:DescribeOrganization",
+                "account:ListRegions"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+  ]
+}
+EOF
+}
